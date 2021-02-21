8572ece3ed86b496fcbd697c769405c61b7a25e4
Evan Driscoll <evaned@gmail.com>
2021-02-21 00:44:59 -0600

    Flip through PDF pages

--- src/views/Music.vue
+++ src/views/Music.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="about">
-    <canvas id="the-canvas"></canvas>
-  </div>
+    <div class="about">
+        <button id="next" @click="next_page">next</button>
+        <button id="prev" @click="prev_page">prev</button>
+        <canvas id="the-canvas"></canvas>
+    </div>
 </template>
 
 <script language="ts">
 import * as Pdf from 'pdfjs-dist';
+
+let g_prev_page = null;
+let g_next_page = null;
 
 async function set_up() {
     const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.entry');
@@ -15,45 +20,103 @@ async function set_up() {
 
     const pdfjsLib = Pdf;
 
-    const loadingTask = pdfjsLib.getDocument(url);
-    loadingTask.promise.then(function(pdf) {
-        console.log('PDF loaded');
-        
-        // Fetch the first page
-        const pageNumber = 1;
-        pdf.getPage(pageNumber).then(function(page) {
-            console.log('Page loaded');
-            
-            const scale = 0.7;
-            const viewport = page.getViewport({scale: scale});
+    let pdfDoc = null;
+    let pageNum = 1;
+    let pageRendering = false;
+    let pageNumPending = null;
+    const scale = 2;
+    const canvas = document.getElementById('the-canvas');
+    const ctx = canvas.getContext('2d');
 
-            // Prepare canvas using PDF page dimensions
-            const canvas = document.getElementById('the-canvas');
-            console.log("canvas")
-            console.log(canvas)
-            const context = canvas.getContext('2d');
-            canvas.height = viewport.height;
+    function renderPage(num) {
+        pageRendering = true;
+        // Using promise to fetch the page
+        pdfDoc.getPage(num).then(function(page) {
+            const viewport = page.getViewport({
+                scale: scale,
+                offsetY: 0,
+            });
+            canvas.height = 750; //viewport.height;
             canvas.width = viewport.width;
 
             // Render PDF page into canvas context
             const renderContext = {
-                canvasContext: context,
+                canvasContext: ctx,
                 viewport: viewport
             };
             const renderTask = page.render(renderContext);
-            renderTask.promise.then(function () {
-                console.log('Page rendered');
+
+            // Wait for rendering to finish
+            renderTask.promise.then(function() {
+                pageRendering = false;
+                if (pageNumPending !== null) {
+                // New page rendering is pending
+                renderPage(pageNumPending);
+                pageNumPending = null;
+                }
             });
         });
-    }, function (reason) {
-        // PDF loading error
-        console.error(reason);
+    }
+
+    // If another page rendering in progress, waits until the rendering is
+    // finised. Otherwise, executes rendering immediately.
+    function queueRenderPage(num) {
+        if (pageRendering) {
+            pageNumPending = num;
+        } else {
+            renderPage(num);
+        }
+    }
+
+    function onPrevPage() {
+        if (pageNum <= 1) {
+            return;
+        }
+        pageNum--;
+        queueRenderPage(pageNum);
+    }
+
+    function onNextPage() {
+        if (pageNum >= pdfDoc.numPages) {
+            return;
+        }
+        pageNum++;
+        queueRenderPage(pageNum);
+    }
+
+    g_prev_page = onPrevPage;
+    g_next_page = onNextPage;
+
+    pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
+        pdfDoc = pdfDoc_;
+        renderPage(pageNum);
     });
 }
 
 set_up();
 
 export default {
-  
+    mounted() {
+        window.addEventListener("keydown", this.key_listener);
+    },
+    unmounted() {
+        window.removeEventListener("keydown", this.key_listener);
+    },
+    methods: {
+        prev_page() {
+            g_prev_page();
+        },
+        next_page() {
+            g_next_page();
+        },
+        key_listener(event) {
+            if (event.key === "ArrowLeft") {
+                this.prev_page();
+            }
+            else if (event.key === "ArrowRight") {
+                this.next_page();
+            }
+        }
+    },
 }
 </script>
